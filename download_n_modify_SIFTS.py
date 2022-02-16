78b3cfdbe98889c37b55982471f51f869dfd358f
ioChris <feidakic@natur.cuni.cz>
2022-02-16 14:39:29 +0100

    add create archive with old SIFTS

--- download_n_modify_SIFTS.py
+++ download_n_modify_SIFTS.py
@@ -17,10 +17,12 @@ Create 2 dictionaries:
 import os
 import gzip
 import wget
+import shutil
+import time
 
 
 # Saving options
-del_old_sifts = 1 # it means processing existing SIFTS file and not downloading the new one
+del_old_sifts = 1 # 0 means processing existing SIFTS file and not downloading the new one
 save_dicts = 1 # if 0, overrides following settings
 
 save_r_spnum = 1    # save dict_R_SPnum dictionary to file (includes readable version w headers)
@@ -31,8 +33,6 @@ if save_dicts == 0:
     save_plaindict = 0
 
 use_old_sifts = False
-
-
 
 
 def root_path():
@@ -46,42 +46,54 @@ def root_path():
 url1 = "http://ftp.ebi.ac.uk/pub/databases/msd/sifts/flatfiles/tsv/pdb_chain_uniprot.tsv.gz"
 
 path_root = root_path() + r'\Documents\Bioinfo_local\Ions\datasets_local\APO_candidates\webserver'
-path1 = path_root + r'\SIFTS'           # Pre compiled files with UniProt PDB mapping   
-#path1 = root_path()  + '\\' + r'ownCloud\Bioinfo_ownCloud\Projects\Ions\Uniprot_PDBchain\autodownload'
-#path1 = r'C:\Users\TopOffice\Documents\GitHub\workDir\files'
+pathSIFTS = path_root + r'\SIFTS'           # Pre compiled files with UniProt PDB mapping
+pathOLD = path_root + r'\oldSIFTS'           # old SIFT files, keep as backup
+#pathSIFTS = root_path() + r'\ownCloud\Bioinfo_ownCloud\Projects\Ions\Uniprot_PDBchain\autodownload'
+#pathSIFTS = r'C:\Users\TopOffice\Documents\GitHub\workDir\files'
 filename = url1.split('/')[-1]
 
 print('Downloading SIFTS files from EBI ftp to local folder')
 print('Download URL:', url1, '\n') # Print the URL that is being checked/downloaded
 print('Target filename: "%s"' % filename) # Print name of file to download
-print('Saving directory:', path1)
+print('Saving directory:', pathSIFTS)
 
-# Check if (old) SIFTS file exists and/or download new file
-if os.path.exists(path1):
-    if os.path.exists(path1 + "\\" + filename):
+# Check if SIFTS directory exists, if not, create it
+if not os.path.exists(pathSIFTS):
+    print('Working directory does not exist, creating new directory\n')
+    try:
+        os.makedirs(pathSIFTS)
+    except:
+        print('Could not create directory: ', pathSIFTS)
+    
+# Check if (old) SIFTS file exists and/or download new file    
+if os.path.exists(pathSIFTS + "\\" + filename):
+    if del_old_sifts == 1:
         
-        if del_old_sifts == 1:
-            print('Deleting existing file')
-            os.remove(path1 + "\\" + filename)
-            
-            # Download new file
-            print('Downloading SIFTS file into ', path1)
-            wget.download(url1, path1)
-        else:
-            print('Using existing SIFTS file')
-            use_old_sifts = True
+        # Archive old SIFTS files before deleting/replacing
+        print('Backing up current SIFTS files')
+        if not os.path.exists(pathOLD):            os.makedirs(pathOLD)
+        time_str = time.strftime("%Y%m%d_%H%M%S", time.gmtime())
+        output_archive_pathname = pathOLD + '\\oldSIFTS_' + time_str
+        shutil.make_archive(output_archive_pathname, 'zip', pathSIFTS)
+        
+        print('Deleting existing file')
+        os.remove(pathSIFTS + "\\" + filename)
+        
+        print('Downloading SIFTS file into ', pathSIFTS)
+        wget.download(url1, pathSIFTS)  # Download new file
     else:
-        # Download new file
-        print('Downloading SIFTS file into ', path1)
-        wget.download(url1, path1)
+        print('Using existing SIFTS file')
+        use_old_sifts = True
 else:
-    print('Working directory does not exist\n')
-        
+    # Download new file
+    print('Downloading SIFTS file into ', pathSIFTS)
+    wget.download(url1, pathSIFTS)
 
+        
 
 # Unzip new .gz file (and overwrite old)
 if not use_old_sifts:
-    with gzip.open(path1 + "\\" + filename ,"rb") as infile, open(path1 + "\\" + filename[:-3] ,"wb") as outfile:
+    with gzip.open(pathSIFTS + "\\" + filename ,"rb") as infile, open(pathSIFTS + "\\" + filename[:-3] ,"wb") as outfile:
         print('Unzipping new file')
         for line in infile:
             outfile.write(line)
@@ -89,7 +101,7 @@ if not use_old_sifts:
 
 
 ## Create reverse version with SPnum and also a simple version of the dict with structchain:uniprot_id
-fileSIFTS = path1 + "\\" + filename[:-3]
+fileSIFTS = pathSIFTS + "\\" + filename[:-3]
 filenameSIFTS = filename[:-3]
 counter_zeroes = 0
 counter_neg = 0
@@ -109,20 +121,19 @@ with open(fileSIFTS, 'r') as infile1:
         structure = line.split("\t")[0]
         chain = line.split("\t")[1]
         uniprot_id = line.split("\t")[2]    #uniprot_id = line.split()[2]
-        SIFTSstructchain = line.split("\t")[0] + line.split("\t")[1]
+        SIFTSstructchain = structure + chain
         
         SP_BEG = int(line.split("\t")[7])
         SP_END = int(line.split("\t")[8][:-1])    # includes \n cause of EOL     
         sp_length = SP_END - SP_BEG     # Calculate length of chain
         
-        # Build basic dictionary with UniProt ID as the key and corresponding PDB structures+chains as values
-        #dict_R_SPnum.setdefault(uniprot_id, []).append(structure + chain)
-        
+
         # Build basic dict with structchain as keys, uniprot IDs as values
         dict_SIFTS[SIFTSstructchain] = uniprot_id
         
         # Append values to the R_SPnum dict (SP_BEG, SP_END, sp_length)
         dict_R_SPnum.setdefault(uniprot_id, []).append(structure+chain+' '+str(SP_BEG)+' '+str(SP_END))
+        #dict_R_SPnum.setdefault(uniprot_id, []).append(structure + chain)  # basic version with UniProt ID as the key and corresponding PDB structures+chains as values
         
         # Count the cases where the length/coverage is zero
         if sp_length == 0:
@@ -139,37 +150,45 @@ print(len(dict_R_SPnum.keys()), sum(len(v) for v in dict_R_SPnum.values()))
 print('Entries with 0 length:\t', counter_zeroes)
 print('Entries with negative length:\t', counter_neg)
 
+'''
+# Discard duplicate values in SIFTS_dict (values)
+print('Removing duplicate entries')
+for key,value in SIFTS_dict.items():
+    SIFTS_dict[key] = list(SIFTS_dict.fromkeys(value))   # preserves the order of values
+print('Done\n')
+'''
+
 ## Save dicts to file (include readable versions)
 
-# R_SPnum dict
+# Reverse dict (dict_R_SPnum)
 if save_r_spnum == 1:
     outfile1 = filenameSIFTS[:-4] + "_REVERSE_SPnum.txt"
     outfile2 = outfile1[:-4] + "_readable.txt"
-    print('Saving output of dict_R_SPnum as ', outfile1, outfile2)
+    print('Saving output of dict_R_SPnum as:\n', outfile1, '\t', outfile2)
     
-    with open (path1 + '\\' + outfile1, 'wt') as out1:
+    with open (pathSIFTS + '\\' + outfile1, 'wt') as out1:
         out1.write(str(dict_R_SPnum))
     
     # Write same dict in a more readable format (new line per key)
-    with open (path1 + '\\' + outfile2, 'wt') as out2:   
+    with open (pathSIFTS + '\\' + outfile2, 'wt') as out2:   
         for key, value, in dict_R_SPnum.items():
             out2.write('{0}: {1}\n'.format(key, value))
         
-# Forward simple dict
+# Forward simple dict (dict_SIFTS)
 if save_plaindict == 1:
     outfile_SIFTS = filenameSIFTS[:-4] + '_dict.txt'
     outfile_SIFTS_readable = outfile_SIFTS[:-4] + "_readable.txt"
-    print('Saving output of dict_SIFTS as ', outfile_SIFTS, outfile_SIFTS_readable)
+    print('Saving output of dict_SIFTS as:\n', outfile_SIFTS, '\t', outfile_SIFTS_readable)
     
-    with open (path1 + '\\' + outfile_SIFTS, 'wt') as out3:
+    with open (pathSIFTS + '\\' + outfile_SIFTS, 'wt') as out3:
         out3.write(str(dict_SIFTS))   
     
     # Write  dict2 in a more readable format (new line per key)
-    with open (path1 + '\\' + outfile_SIFTS_readable, 'wt') as out4:   
+    with open (pathSIFTS + '\\' + outfile_SIFTS_readable, 'wt') as out4:   
         #header = '#HEADER: HOLO_chain SP_BEG SP_END : APO_chain SP_BEG SP_END SP_LEN overlap_len %overlap\n'
         out4.write(header1)
         out4.write(header2)
         for key, value, in dict_SIFTS.items():
             out4.write('{0}: {1}\n'.format(key, value))
 
-print('Done')
+print('All done')
